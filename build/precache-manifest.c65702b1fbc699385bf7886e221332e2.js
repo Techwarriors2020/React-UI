5ea8ef60bb10736a100b6485674f63a7f6bb85bb
deepak <techwinner20@gmail.com>
2020-05-18 16:06:52 -0700

    build

---
+++ build/precache-manifest.c65702b1fbc699385bf7886e221332e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96569edf27aec3becfc2202c3a914054",
+    "url": "/ReactApp/index.html"
+  },
+  {
+    "revision": "6af827f799a4332609c6",
+    "url": "/ReactApp/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "57e437ca08f414b4731c",
+    "url": "/ReactApp/static/js/2.c509455a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactApp/static/js/2.c509455a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6af827f799a4332609c6",
+    "url": "/ReactApp/static/js/main.1d2fc554.chunk.js"
+  },
+  {
+    "revision": "7a258e049a354184a124",
+    "url": "/ReactApp/static/js/runtime-main.e16aad01.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/ReactApp/static/media/logo.ee7cd8ed.svg"
+  }
+]);
