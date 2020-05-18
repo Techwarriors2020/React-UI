5de927e3bd9ed17b2041d261e3dad68a93ef43aa
deepak <techwinner20@gmail.com>
2020-05-18 14:58:44 -0700

    Updates

---
+++ precache-manifest.7470cde80850546af17b6e6fcdb4a086.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22472c3a6f8fcdda9c18ef775ae3ae28",
+    "url": "/React-UI/index.html"
+  },
+  {
+    "revision": "86b82d8e3a4a6269da9b",
+    "url": "/React-UI/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "57e437ca08f414b4731c",
+    "url": "/React-UI/static/js/2.c509455a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-UI/static/js/2.c509455a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86b82d8e3a4a6269da9b",
+    "url": "/React-UI/static/js/main.f524442d.chunk.js"
+  },
+  {
+    "revision": "70efd9375fce688a61c8",
+    "url": "/React-UI/static/js/runtime-main.af0be818.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-UI/static/media/logo.5d5d9eef.svg"
+  }
+]);
