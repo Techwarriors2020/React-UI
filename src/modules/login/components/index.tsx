0162b8891cb1f682963e1639446dd60234f18b1e
deepak <techwinner20@gmail.com>
2020-05-22 22:10:04 +0530

    changes

--- src/modules/login/components/index.tsx
+++ src/modules/login/components/index.tsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 //import classes from './Button.css';
-import JsonEditor from './react-json-editor';
+
 
 interface ILoginProps {
     children?: React.ReactNode;
@@ -13,7 +13,7 @@ const Login: React.FC<ILoginProps> = ({ children, ...props }) => {
     return (
         <div {...props} >
           <p>login page under construction</p>
-	<JsonEditor></JsonEditor>
+	
         </div>
     );
 };
