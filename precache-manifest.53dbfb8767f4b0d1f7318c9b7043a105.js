b65ee54f94a7c2d61776e492255d7223aaa7a18d
deepak <techwinner20@gmail.com>
2020-05-23 01:04:48 +0530

    Updates

---
+++ precache-manifest.53dbfb8767f4b0d1f7318c9b7043a105.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b65c464e75d087076f7c413c1fd28d1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ba55b4073ae55967838",
+    "url": "/static/css/2.9688197f.chunk.css"
+  },
+  {
+    "revision": "7abe15fd20c4116051ce",
+    "url": "/static/css/main.da6663f7.chunk.css"
+  },
+  {
+    "revision": "0ba55b4073ae55967838",
+    "url": "/static/js/2.2256abe8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2256abe8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7abe15fd20c4116051ce",
+    "url": "/static/js/main.21096620.chunk.js"
+  },
+  {
+    "revision": "b1789a7d157f23e2c445",
+    "url": "/static/js/runtime-main.0f099c74.js"
+  }
+]);
--- src/constants/static.constants.js
+++
@@ -1,3 +0,0 @@
-export const phoneNumberCountryCode = {
-    IN: '+91'
-}
