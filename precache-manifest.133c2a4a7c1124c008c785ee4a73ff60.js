c62c973567e8d9d8514f6e82e7607e4a8c6fc5ef
deepak <techwinner20@gmail.com>
2020-05-23 18:19:14 +0530

    Updates

---
+++ precache-manifest.133c2a4a7c1124c008c785ee4a73ff60.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67f6f336b87a22364f0ba8647d4a2c45",
+    "url": "/signup/index.html"
+  },
+  {
+    "revision": "1579f20f4d00c1787c37",
+    "url": "/signup/static/css/2.bc34697d.chunk.css"
+  },
+  {
+    "revision": "d7b3ceac4c1cbb0eb752",
+    "url": "/signup/static/css/main.319f1ad5.chunk.css"
+  },
+  {
+    "revision": "1579f20f4d00c1787c37",
+    "url": "/signup/static/js/2.72152d84.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/signup/static/js/2.72152d84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7b3ceac4c1cbb0eb752",
+    "url": "/signup/static/js/main.12030298.chunk.js"
+  },
+  {
+    "revision": "8f0aab4559c008d5de4b",
+    "url": "/signup/static/js/runtime-main.483d1d1e.js"
+  },
+  {
+    "revision": "37c135a25d31dbb594cf866456fd1431",
+    "url": "/signup/static/media/logo.37c135a2.png"
+  }
+]);
