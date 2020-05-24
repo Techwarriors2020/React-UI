2a48cc2ea89fdc4579c1ae97fa68e4ebc9d89732
deepak <techwinner20@gmail.com>
2020-05-24 19:07:38 +0530

    cards changes

--- src/modules/landingpage/components/landingPage.component.tsx
+++ src/modules/landingpage/components/landingPage.component.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import LeftPanel from  '../../../common-components/leftPanel.component';
 import RightPanel from  '../../../common-components/rightPanel.component';
-import Cards,{CardBody} from '../../../stories/components/molecules/cards/cards';
+import Cards,{CardBody, CardHeader} from '../../../stories/components/molecules/cards/cards';
 import * as constants from '../../../constants';
 
 interface ILandingPageProps {
@@ -19,8 +19,9 @@ const LandingPage: React.FC<ILandingPageProps> = ({ children, ...props }) => {
            {/* later we can add scroll if need */}
             {constants.landingPage.cardsArr.map(function(item, i){
         return   <Cards title='card' key ={i} className='ekutumb-card'>
+                    <CardHeader className="ekutumb-card-header"><span>{item.card}</span></CardHeader>
                     <CardBody className='ekutumb-card-body ekutumb-bg-purple'>
-                        <span>{item.card}</span>
+
                     </CardBody>
                     </Cards>;
               })}
